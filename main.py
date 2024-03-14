267bcbd6a0af5b7c6f8bcf731759ed1453b21f7e
Romain Pinsolle <romain.pins@gmail.com>
2024-03-14 08:36:12 +0100

    préadd de tout les but + full but1

--- main.py
+++ main.py
@@ -16,11 +16,20 @@ app.add_middleware(
 
 
 group_links = {
-    "but1_g1": "",
-    "but1_g2": "",
+    "but1_g1": "https://ade.univ-pau.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?resources=4962,4989&projectId=6&calType=ical&nbWeeks=99",
+    "but1_g2": "https://ade.univ-pau.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?resources=4963,4990&projectId=6&calType=ical&nbWeeks=99",
     "but1_g3": "https://ade.univ-pau.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?resources=4964,4991&projectId=6&calType=ical&nbWeeks=99",
-    "but1_g4": ""
-    
+    "but1_g4": "https://ade.univ-pau.fr/jsp/custom/modules/plannings/anonymous_cal.jsp?resources=4965,4992&projectId=6&calType=ical&nbWeeks=99",
+    "s3_g1": "",
+    "s3_altg1": "",
+    "s3_altg2": "",
+    "s4_cyber": "",
+    "s4_rom" : "", 
+    "s4_pilpro" : "",
+    "but3_cyber1" : "",
+    "but3_cyber2" : "", 
+    "but3_rom" : "", 
+    "but3_pilpro" : "", 
 }
 
 app = FastAPI()
